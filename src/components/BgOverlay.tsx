20c5f43e2b36651d803d9b78485217849d2737d2
Mikhailo Benesko <68901039+mikhaben@users.noreply.github.com>
2023-12-18 23:52:02 +0100

    Added second circle amimation

--- src/components/BgOverlay.tsx
+++ src/components/BgOverlay.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
 import { Animated, Easing, View } from 'react-native'
 
 import { ThemeContext } from '../context/ThemeProvider'
@@ -8,41 +8,74 @@ const AnimatedFlow = Animated.createAnimatedComponent(View)
 const BgOverlay = (): React.ReactNode => {
   const { theme } = useContext(ThemeContext)
   const circleSize = new Animated.Value(0)
+  const circleSize2 = new Animated.Value(0)
   const opacity = new Animated.Value(1)
+  const isMounted = useRef(false)
 
   useEffect(() => {
-    Animated.timing(circleSize, {
-      toValue: 1000,
-      duration: 700,
-      useNativeDriver: false,
-      easing: Easing.inOut(Easing.ease)
-    }).start(() => {
-      Animated.timing(opacity, {
-        toValue: 0,
-        duration: 300,
-        useNativeDriver: false
+    if (isMounted.current) {
+      // First Circle Animation
+      Animated.timing(circleSize, {
+        toValue: 1000,
+        duration: 700,
+        useNativeDriver: false,
+        easing: Easing.inOut(Easing.ease)
       }).start(() => {
-        circleSize.setValue(0)
-        opacity.setValue(1)
+        Animated.timing(opacity, {
+          toValue: 0,
+          duration: 300,
+          useNativeDriver: false
+        }).start(() => {
+          circleSize.setValue(0)
+          opacity.setValue(1)
+        })
       })
-    })
+
+      // Second Circle Animation
+      Animated.timing(circleSize2, {
+        toValue: 1000,
+        duration: 700,
+        useNativeDriver: false,
+        easing: Easing.inOut(Easing.ease)
+      }).start(() => {
+        circleSize2.setValue(0)
+      })
+    } else {
+      isMounted.current = true
+    }
   }, [theme])
 
   return (
-    <AnimatedFlow
-      style={{
-        width: circleSize,
-        height: circleSize,
-        borderRadius: circleSize,
-        position: 'absolute',
-        right: '50%',
-        top: '50%',
-        transform: [{ translateX: 500 }, { translateY: -500 }],
-        zIndex: 100,
-        opacity
-      }}
-      className={theme.backgroundColor}
-    />
+    <>
+      <AnimatedFlow
+        style={{
+          position: 'absolute',
+          right: '50%',
+          top: '50%',
+          zIndex: 100,
+          width: circleSize,
+          height: circleSize,
+          borderRadius: circleSize,
+          opacity,
+          transform: [{ translateX: 500 }, { translateY: -500 }]
+        }}
+        className={theme.backgroundColor}
+      />
+
+      <AnimatedFlow
+        style={{
+          position: 'absolute',
+          left: '-50%',
+          bottom: '-50%',
+          zIndex: 99,
+          width: circleSize2,
+          height: circleSize2,
+          borderRadius: circleSize2,
+          transform: [{ translateX: 0 }, { translateY: 0 }]
+        }}
+        className={theme.accentBackgroundColor}
+      />
+    </>
   )
 }
 
